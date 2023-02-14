3a2a21ea15c36cef7d84ed5bc71d02b4c5c0edbc
nicolatournay <nicolatournay@outlook.com>
2023-02-14 11:42:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var select = document.querySelector('select');
+
+fetch("https://public.opendatasoft.com/api/records/1.0/search/?dataset=countries-codes&q=&rows=249")
+    .then(response => response.json())
+    .then(data => {
+        data.records.forEach(record => {
+            var country = record.fields.label_fr;
+            var iso_code = record.fields.iso2_code;
+            select.innerHTML += `
+                <option value="${iso_code}">${country}</option>
+            `;
+        });;
+    })
+    .catch(error => {    console.error("Erreur lors de la récupération des données :", error);
+});
+
+var apiKey = "5bbccc6c983e2172d96f63d9873c9f5a";
+
+select.addEventListener("change", function() {
+    var value = select.value;
+    console.log(value);
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=1&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {    console.log(data);  })
+        .catch(error => {    console.error("Erreur lors de la récupération des données :", error);
+    });
+});
